07828e8bb61e0a0f00e30f756a638c6d28c276ec
carlalennon lennon <carlalennon@gmail.com>
2024-04-09 19:42:29 +0000

    Add product detail URL

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 # Create your views here.
 
@@ -13,10 +13,10 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """ Returns all products page """
+    """ Returns page with detailed information about selected product  """
     
-    products = Product.objects.all()
+    product = get_object_or_404(Product, pk=product_id)
     context = {
-        'products': products,
+        'product': product,
     }
     return render(request, 'products/product_detail.html', context)
