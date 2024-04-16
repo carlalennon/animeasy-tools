90057ef685fe3a6cba29a53656843f0514015ffd
carlalennon lennon <carlalennon@gmail.com>
2024-04-16 20:47:39 +0000

    Debugging sort selector

--- products/views.py
+++ products/views.py
@@ -8,8 +8,8 @@ from .models import Product, Category
 
 
 def all_products(request):
-    """ Returns all products page """
-    
+    """ A view to show all products, including sorting and search queries """
+
     products = Product.objects.all()
     query = None
     categories = None
@@ -25,36 +25,35 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
-                
             if 'direction' in request.GET:
                 direction = request.GET['direction']
-                products = products.order_by(f'{sortkey}{direction}')
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-            
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, 'Please enter a keyword, name or software')
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-                
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
-    
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
     }
+
     return render(request, 'products/products.html', context)
 
 
