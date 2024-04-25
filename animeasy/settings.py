5e40c8c148ff158ec65bf96036d82c51468f19a9
carlalennon lennon <carlalennon@gmail.com>
2024-04-25 22:43:27 +0000

    Install crispy forms

--- animeasy/settings.py
+++ animeasy/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'profiles',
     'bag',
     'checkout',
+    
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +65,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'animeasy.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -79,6 +83,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
+            ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ],
         },
     },
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse 
+from django.contrib import messages 
 
-# Create your views here.
+from .forms import OrderForm
+
+def checkout(request):
+    bag = request.soession.get('bag', {})
+    if not bag: 
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
+    return render(request, 'checkout/checkout.html')
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    } 
+    
+    return render(request, template, context)
