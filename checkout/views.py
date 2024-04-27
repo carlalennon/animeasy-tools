0abd8327d376429d0d7a01255810becd5bcd79ea
carlalennon lennon <carlalennon@gmail.com>
2024-04-27 23:16:27 +0000

    Start adding fields to checkout form

--- checkout/views.py
+++ checkout/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from .forms import OrderForm
 
 def checkout(request):
-    bag = request.soession.get('bag', {})
+    bag = request.session.get('bag', {})
     if not bag: 
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
