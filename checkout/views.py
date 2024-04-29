ec8abb3d586e1b31ff2e1499654400ba6d4a4187
carlalennon lennon <carlalennon@gmail.com>
2024-04-29 19:22:32 +0000

    Stripe accepts a test payment

--- checkout/views.py
+++ checkout/views.py
@@ -28,11 +28,16 @@ def checkout(request):
     print(intent)
     
     order_form = OrderForm()
+    
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+        
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51PAWdpEtLZegFfEszCWKdGBF72rdTrTWr8Uyua5FCmKsEmtFvkXw0XereQKbHJ8VnM8RsnVOXyrulPAXJFnxuXWR00DOeUW99Q',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     } 
     
     return render(request, template, context)
