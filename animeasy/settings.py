46e619056aac9de4265bcd6607931b4ebc2d95a5
carlalennon lennon <carlalennon@gmail.com>
2024-04-29 19:03:33 +0000

    Add secret key elements to env

--- animeasy/settings.py
+++ animeasy/settings.py
@@ -179,3 +179,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Stripe
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
