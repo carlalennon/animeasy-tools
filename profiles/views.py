b657be1e33f26c74ff25cf59e417fb1c86b51c10
carlalennon lennon <carlalennon@gmail.com>
2024-04-30 21:27:12 +0000

    Added profile html and update model

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def profile(request):
+    """ Display user profile """
+    
+    template = 'profiles/profile.html'
+    
+    context = {
+        'on_profile_page': True
+    }
+    
+    return render(request, template, context)   
