85ee1d03d28bed18a64366db289ea2d021146318
carlalennon lennon <carlalennon@gmail.com>
2024-05-05 19:59:33 +0000

    Add product management view

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Category
+from .forms import ProductForm
 # Create your views here.
 
 
@@ -65,3 +66,17 @@ def product_detail(request, product_id):
         'product': product,
     }
     return render(request, 'products/product_detail.html', context)
+
+def add_product(request):
+    if request.user.is_authenticated:
+        if not request.user.is_superuser:
+            messages.error(request, 'Sorry, only store owners can do that.')
+            return redirect(reverse('home'))
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }   
+    
+    return render(request, template, context)
