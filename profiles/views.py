ea9d3cfd34ba5f67c941b4a869de4ccf9eed3b78
carlalennon lennon <carlalennon@gmail.com>
2024-05-05 00:19:59 +0000

    Add user information to profile in accordian

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from .forms import UserProfileForm
 
 # Create your views here.
 def profile(request):
     """ Display user profile """
     profile = get_object_or_404(UserProfile, user=request.user)
     
+    form = UserProfileForm(instance=profile)
     template = 'profiles/profile.html'
+    orders = profile.orders.all()
     
     context = {
+        'form': form,
         'profile': profile,
+        'orders' : orders,
     }
     
     return render(request, template, context)   
