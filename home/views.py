c4122eb0f9169b65348b3e4299fab5980ed9187a
carlalennon lennon <carlalennon@gmail.com>
2024-05-07 22:34:39 +0000

    Redo toasts

--- home/views.py
+++ home/views.py
@@ -5,6 +5,11 @@ from newsletter.forms import SubscriberForm
 # Create your views here.
 
 def index(request):
+    messages.warning(request, 'This is a warning message')
+    messages.error(request, 'This is an error message')
+    messages.success(request, 'This is a success message')
+    messages.info(request, 'This is an info message')
+
     form = SubscriberForm()
     """ Returns index page """
     if request.method == 'POST':
