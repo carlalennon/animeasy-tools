37bac7234f9c6d37dde5bc1b9d3317f3c1a068cf
carlalennon lennon <carlalennon@gmail.com>
2024-05-10 22:13:57 +0000

    Make complaint content field mandatory

--- contact/models.py
+++ contact/models.py
@@ -6,7 +6,7 @@ class Ticket(models.Model):
     email = models.EmailField(null=True, max_length=254)
     date_received = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = models.TextField(null=True)
     status = models.CharField(max_length=10, choices=[('pending', 'Pending'), ('resolved', 'Resolved')], default='pending')
 
     def __str__(self):
