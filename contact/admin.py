1a50c93c2b0d3940f3f50c4019c500892ed6b9a6
carlalennon lennon <carlalennon@gmail.com>
2024-05-12 20:57:59 +0000

    Add ticket replies in admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import Ticket 
+from .models import Ticket, TicketReply
 
 admin.site.register(Ticket)
+
+class TicketReplyAdmin(admin.ModelAdmin):
+    """
+    Label replies by ticket in admin view
+    """
+    def __str__(self):
+        return f"Reply to {self.ticket.name}"
+
+admin.site.register(TicketReply, TicketReplyAdmin)
