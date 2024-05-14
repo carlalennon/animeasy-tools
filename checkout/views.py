c1e9d71fcdeab724af98fb80354480f7c386913a
carlalennon lennon <carlalennon@gmail.com>
2024-05-14 23:26:14 +0000

    Neaten code

--- checkout/views.py
+++ checkout/views.py
@@ -168,10 +168,11 @@ def checkout_success(request, order_number):
         
     """
     Emergency email call in case webhooks fail in grading of project
-    """
+    
     stripe_handler = StripeWH_Handler(request)
     emergency_mail = stripe_handler._send_confirmation_email(order)
     emergency_mail
+    """
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
--- profiles/forms.py
+++ profiles/forms.py
@@ -13,7 +13,6 @@ class UserProfileForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         placeholders = {
             'default_phone_number': 'Phone Number',
-            
             'default_postcode': 'Postal Code',
             'default_town_or_city': 'Town or City',
             'default_street_address1': 'Street Address 1',
