838397ca5e21100a8832fda9fbeb199008709c69
carlalennon lennon <carlalennon@gmail.com>
2024-07-08 23:59:54 +0000

    Add docstrings2

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,7 +1,16 @@
+"""
+Configuration class for the 'checkout' app.
+
+This class defines the configuration for the 'checkout' app in Animeasy.
+It registers the 'checkout.signals' module to be imported when the app is ready.
+"""
+
 from django.apps import AppConfig
 
-
 class CheckoutConfig(AppConfig):
+    """
+    Configuration for the 'checkout' app
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
 
