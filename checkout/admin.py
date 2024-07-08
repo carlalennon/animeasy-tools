b6eae3cf55058744cf84493d564bb44dbdf6c2c7
carlalennon lennon <carlalennon@gmail.com>
2024-07-08 23:55:17 +0000

    Remove whitespaces and extra newlines. Add docstring to top of file and within classes

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,40 +1,48 @@
+"""
+Sets up the admin panel for orders in the checkout app
+"""
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
-
-class OrderLineItemAdminInline(admin.TabularInline):    
+class OrderLineItemAdminInline(admin.TabularInline):
+    """
+    Shows the line items in the admin panel
+    """
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
 
 class OrderAdmin(admin.ModelAdmin):
+    """
+    Order admin panel
+    """
     inlines = (OrderLineItemAdminInline,)
     readonly_fields = (
-        'order_number', 
-        'date', 
-        'order_total', 
+        'order_number',
+        'date',
+        'order_total',
         'grand_total',
         'original_bag',
         'stripe_pid',
         )
-    
+
     fields = (
-        'order_number', 
+        'order_number',
         'user_profile',
-        'date', 'full_name', 
-        'email', 
-        'phone_number', 
-        'country', 
-        'postcode', 
-        'town_or_city', 
-        'street_address1', 
-        'street_address2', 
-        'county', 
-        'order_total', 
+        'date', 'full_name',
+        'email',
+        'phone_number',
+        'country',
+        'postcode',
+        'town_or_city',
+        'street_address1',
+        'street_address2',
+        'county',
+        'order_total',
         'grand_total',
         'original_bag',
         'stripe_pid',
         )
-    
+
     list_display = (
         'order_number',
         'date',
@@ -43,9 +51,7 @@ class OrderAdmin(admin.ModelAdmin):
         'grand_total',
         'country',
     )
-    
+
     ordering = ('-date',)
 
-
 admin.site.register(Order, OrderAdmin)
-
