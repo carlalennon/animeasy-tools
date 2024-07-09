7fe41ffda92194a85baecc6c499d71408309becf
carlalennon lennon <carlalennon@gmail.com>
2024-07-09 00:31:54 +0000

    Remove whitespaces, add newline2

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,6 +1,8 @@
+"""
+A file with listeners that will update the order total when a line item is created, updated or deleted.
+"""
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-
 from .models import OrderLineItem
 
 @receiver(post_save, sender=OrderLineItem)
@@ -9,12 +11,11 @@ def update_on_save(sender, instance, created, **kwargs):
     Update order total on lineitem update/create
     """
     instance.order.update_total()
-    
-    
+
+
 @receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, created, **kwargs):
     """
     Update order total on lineitem delete
     """
     instance.order.update_total()
-    
