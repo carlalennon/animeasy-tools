ef8113b88821e787454901b6f80a629faf7b041d
carlalennon lennon <carlalennon@gmail.com>
2024-07-09 19:28:23 +0000

    Refactor docstring

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,5 +1,6 @@
 """
-A file with listeners that will update the order total when a line item is created, updated or deleted.
+A file with listeners that will update the order total when a line
+item is created, updated or deleted.
 """
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
