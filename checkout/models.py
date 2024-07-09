87f7e39d370e4b719775ea28ad6208bfb4634bd3
carlalennon lennon <carlalennon@gmail.com>
2024-07-09 00:20:25 +0000

    Refactor long lines and remove unused import

--- checkout/models.py
+++ checkout/models.py
@@ -1,7 +1,6 @@
 import uuid
 from django.db import models
 from django.db.models import Sum
-from django.conf import settings
 from products.models import Product
 from profiles.models import UserProfile
 
@@ -44,7 +43,8 @@ class Order(models.Model):
         self.save()
 
     def save(self, *args, **kwargs):
-        """ Override the original save method to set the order number if it hasn't been set already"""
+        """ Override the original save method to set the order number
+        if it hasn't been set already"""
         if not self.order_number:
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
@@ -53,13 +53,26 @@ class Order(models.Model):
         return self.order_number
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    order = models.ForeignKey(
+        Order,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        related_name='lineitems'
+        )
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    lineitem_total = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=False,
+        blank=False,
+        editable=False
+        )
 
     def save(self, *args, **kwargs):
-        """ Override the original save method to set the lineitem total and update the order total"""
+        """ Override the original save method to set the lineitem total 
+        and update the order total"""
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
