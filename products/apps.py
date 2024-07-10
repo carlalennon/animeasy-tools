94ded8d1f16fb1f3cb31dab731c01c1e9caf0176
carlalennon lennon <carlalennon@gmail.com>
2024-07-10 21:40:37 +0000

    Add docstrings

--- products/apps.py
+++ products/apps.py
@@ -1,6 +1,11 @@
+"""
+Set up product configuration app for store products
+"""
 from django.apps import AppConfig
 
-
 class ProductsConfig(AppConfig):
+    """
+    PRODUCT CONFIGURATION
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'products'
