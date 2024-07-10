8c698409d1fddf24654ccb875d78547ab8aaa76f
carlalennon lennon <carlalennon@gmail.com>
2024-07-10 22:12:42 +0000

    Remove whitespace, change docstring to comment

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,15 +1,19 @@
+"""
+Forms for user profiles
+"""
 from django import forms
 from .models import UserProfile
 
 class UserProfileForm(forms.ModelForm):
+    """
+    Users can change their default billing information
+    """
     class Meta:
         model =UserProfile
         exclude = ('user', )
-        
+
     def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated labels and set autofocus on first field
-        """
+        #Add placeholders and classes, remove auto-generated labels and set autofocus on first field
         super().__init__(*args, **kwargs)
         placeholders = {
             'default_phone_number': 'Phone Number',
@@ -19,7 +23,7 @@ class UserProfileForm(forms.ModelForm):
             'default_street_address2': 'Street Address 2',
             'default_county': 'County',
         }
-        
+
         self.fields['default_phone_number'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if field != 'default_country':
