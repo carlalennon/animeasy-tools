b14d54c847860598546e56d04dbd8e868b42c30a
carlalennon lennon <carlalennon@gmail.com>
2024-07-10 22:10:15 +0000

    Add docstring

--- profiles/apps.py
+++ profiles/apps.py
@@ -1,6 +1,11 @@
+"""
+Set up apps for user profiles
+"""
 from django.apps import AppConfig
 
-
 class ProfilesConfig(AppConfig):
+    """
+    App configuration for profiles
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profiles'
