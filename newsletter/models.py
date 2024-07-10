c2f2a13560154a75c33115b08b629134f474c355
carlalennon lennon <carlalennon@gmail.com>
2024-07-10 21:11:49 +0000

    Add docstring

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,6 +1,12 @@
+"""
+Models for newsletter and subscribers
+"""
 from django.db import models
 
-class Subscriber(models.Model):
+class Subscriber(models.Model):#
+    """
+    Model for people subscribed to the newsletter
+    """
     email = models.EmailField(null=True, max_length=254)
     date_subscribed = models.DateTimeField(auto_now_add=True)
 
@@ -8,8 +14,10 @@ class Subscriber(models.Model):
         return self.email
 
 
-
 class Newsletter(models.Model):
+    """
+    Model for the newsletter
+    """
     title = models.CharField(max_length=200, null=True)
     content = models.TextField(null=True)
     date_created = models.DateTimeField(auto_now_add=True)
