6f7862826e74c3f33756df52814c9520386b44cd
carlalennon lennon <carlalennon@gmail.com>
2024-07-10 21:03:22 +0000

    Add docstrings

--- newsletter/apps.py
+++ newsletter/apps.py
@@ -1,6 +1,12 @@
+"""
+Sets up the app for the newsletter app
+"""
 from django.apps import AppConfig
 
 
 class NewsletterConfig(AppConfig):
+    """
+    Create big auto field
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'newsletter'
