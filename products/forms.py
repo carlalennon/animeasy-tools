07dabb7c7e2776fbfc6a0c63664c47a1218fe920
carlalennon lennon <carlalennon@gmail.com>
2024-07-10 21:42:43 +0000

    Add docstrings, remove whitespaces

--- products/forms.py
+++ products/forms.py
@@ -1,21 +1,25 @@
-from django import forms 
+"""
+Forms for the product app
+"""
+from django import forms
 from .models import Product, Category
 from .widgets import CustomClearableFileInput
 
-
 class ProductForm(forms.ModelForm):
-    
+    """
+    Form for product model
+    """
     class Meta:
         model = Product
         fields = '__all__'
-        
+
     image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
-        
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-        
+
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'stripe-style'
