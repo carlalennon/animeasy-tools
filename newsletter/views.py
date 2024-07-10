3b2775d5786a694e837896a40c495250fb2d8f82
carlalennon lennon <carlalennon@gmail.com>
2024-07-10 21:33:52 +0000

    Group django imports2

--- newsletter/views.py
+++ newsletter/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 # from django.core.paginator import Paginator
 from django.contrib import messages
 from django.core.mail import send_mail
-from django_pandas.io import read_frame
 from django.contrib.auth.decorators import login_required
+from django_pandas.io import read_frame
 from .models import Subscriber, Newsletter
 from .forms import NewsletterForm
 
