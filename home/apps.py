8faf34c542ac5b5b53d0678c8b7f2dd0e11583bb
carlalennon lennon <carlalennon@gmail.com>
2024-07-10 19:49:01 +0000

    Added docstrings

--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,12 @@
+"""
+Sets up the forms for the home page newsletter box
+"""
 from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
+    """
+    Sets up the newsletter subscribe box
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
