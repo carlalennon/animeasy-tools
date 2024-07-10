3edfe38d59b2d7f5984e94133fe26eb4202c6b11
carlalennon lennon <carlalennon@gmail.com>
2024-07-10 21:47:47 +0000

    Add docstrings2

--- products/models.py
+++ products/models.py
@@ -1,6 +1,12 @@
+"""
+Models for the product app
+"""
 from django.db import models
 
 class Category(models.Model):
+    """
+    Catergory model for products
+    """
     class Meta:
         verbose_name_plural = 'Categories'
     name = models.CharField(max_length=254)
@@ -13,6 +19,9 @@ class Category(models.Model):
         return self.friendly_name
 
 class Product(models.Model):
+    """
+    Product model for store products
+    """
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length = 254, null=True, blank=True)
     name = models.CharField(max_length = 254)
@@ -21,6 +30,6 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    
+
     def __str__(self):
         return self.name
